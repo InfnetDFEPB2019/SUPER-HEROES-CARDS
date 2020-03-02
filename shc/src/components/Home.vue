023649fe543dd220c1a1a1025ae13a8f55191683
leoewbankinfnet <56918110+leoewbankinfnet@users.noreply.github.com>
2020-03-02 09:32:36 -0300

    update

--- shc/src/components/Home.vue
+++ shc/src/components/Home.vue
@@ -9,28 +9,16 @@
           <b-navbar-nav>
             <b-nav-item href="#">Home</b-nav-item>
             <b-nav-item href="#">Jogar</b-nav-item>
-            <b-nav-item href="#">Cadastro</b-nav-item>
-            <b-nav-item href="#">Cartas</b-nav-item>
-            <b-nav-item href="#">Sobre</b-nav-item>
-            <b-nav-item href="#">Contatos</b-nav-item>
-
-          </b-navbar-nav>
-
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-            <b-nav-form>
-              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form>
-
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
+            <b-nav-item-dropdown center>
               <template v-slot:button-content>
                 <em>User</em>
               </template>
               <b-dropdown-item href="#">Profile</b-dropdown-item>
               <b-dropdown-item href="#">Sign Out</b-dropdown-item>
             </b-nav-item-dropdown>
+            <b-nav-item href="#">Cartas</b-nav-item>
+            <b-nav-item href="#">Sobre</b-nav-item>
+            <b-nav-item href="#">Contatos</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
@@ -59,42 +47,35 @@ export default {
   font-family: "theboldfont";
   src: url("../assets/theboldfont.ttf");
 }
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 
 #logo{
-  width:25%;
+  width:100px;
   margin-left: 0;
-  border-radius: 50%;
+  border-radius: 99%;
   align-items:left;
 }
-
-.navbar-brand{
-  width:15%;
+.navbar{
+  position: sticky
+}
+ul.navbar-nav {
+  display: flex;
+  justify-content: space-around;
+  width: 80%;
+  align-items:center; 
+  font-size: 25px
 }
 
 a{
-  font-size: 22px;
+  font-size: 25px;
 }
 #nav-collapse{
   font-family: Beckman-free;
-  justify-content: space-around
+  justify-content: space-around;
 }
 #Logo-titulo{
   font-family: Conversation;
-  font-size: 50px;
-  margin-right: 5%
+  font-size: 60px;
+  padding: 10px
 }
+
 </style>
