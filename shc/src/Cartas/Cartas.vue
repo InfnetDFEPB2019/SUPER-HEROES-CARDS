b83b8045a4149d8594a1f36a0b5845a6e8654085
leoewbankinfnet <56918110+leoewbankinfnet@users.noreply.github.com>
2020-03-09 19:55:11 -0300

    paginas criadas

---
+++ shc/src/Cartas/Cartas.vue
@@ -0,0 +1,47 @@
+<template>
+   <div id="Header_Nav">
+      <Header/>
+
+      
+
+    </div>
+    
+        
+    
+
+
+</template>
+
+<script>
+
+import Header from '../components/Header'
+
+
+export default {
+  name: 'Cartas',
+  components: {
+    Header,
+    
+  }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@font-face {
+  font-family:"Beckman-free";
+  src: url("../assets/Beckman-Free.otf");
+}
+@font-face {
+  font-family: "Conversation";
+  src: url("../assets/Conversation.otf");
+}
+@font-face {
+  font-family: "theboldfont";
+  src: url("../assets/theboldfont.ttf");
+}
+
+
+</style>
--- shc/src/router.js
+++ shc/src/router.js
@@ -1,11 +1,46 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+
+import Cadastro from './Cadastro/Cadastro.vue'
+import Home from './Home/Home.vue'
+import Login from './Login/Login.vue'
+import Cartas from './Cartas/Cartas.vue'
+import Jogar from './Jogar/Jogar.vue'
+
+
+
+
 Vue.use(VueRouter);
 
 export default new VueRouter ({
     mode:"history",
     routes: [
+        {
+            path: '/cadastro',
+            name:'cadastro',
+            component:Cadastro
+        },
+        {
+            path: '/',
+            name:'home',
+            component:Home
+        },
+        {
+            path: '/login',
+            name:'login',
+            component:Login
+        },
+        {
+            path: '/cartas',
+            name:'cartas',
+            component:Cartas
+        },
+        {
+            path: '/jogar',
+            name:'jogar',
+            component:Jogar
+        },
         
     ]
 })
