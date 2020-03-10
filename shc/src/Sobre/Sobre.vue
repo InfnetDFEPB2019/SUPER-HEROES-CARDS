fdc8791014ff00c081e2f088eac55f99ff3978b1
leoewbankinfnet <56918110+leoewbankinfnet@users.noreply.github.com>
2020-03-09 21:46:51 -0300

    Update Sobre.vue

--- shc/src/Sobre/Sobre.vue
+++ shc/src/Sobre/Sobre.vue
@@ -6,7 +6,7 @@
 
       <section id='texto'>
           
-            <h2> Um jogo divertido de super heróis </h2>
+            <h2> Um projeto agregador de conhecimentos, experiências e desafios </h2>
           <p>
               Esse jogo foi construído com o intuito de integrar todos os conhecimentos obtidos ao longo do bloco de Front-End.
               Durante esse bloco aprendemos a utilizar o JavaScript, HTML, CSS e frameworks que auxiliam na programação de sites funcionais
@@ -14,7 +14,7 @@
               O caminho para a construção do site foi complexo, cheio de pedras e diversos obstáculos, muitas vezes o conhecimento obtido em sala
               não foi o suficiente e o grupo teve que buscar informações além do já ensinado. 
           </p>
-          <br>
+     
           <br>
           <p>
               A equipe de desenvolvimento desse projeto foi composta por 4 pessoas: Leonardo Ewbank, Nathan Borges, Marcos Aurelios, Matheus Matos. 
@@ -63,8 +63,16 @@ export default {
   src: url("../assets/theboldfont.ttf");
 }
 h1 {
-  font-size:40px;
+  font-size:50px;
   margin-top:10px;
+  color:green;
+}
+
+#texto{
+  margin:0 auto;
+  display:block;
+  width: 1000px;
+  background-color:pink;
 }
 
 
