b516234a7ba933610b7d831b2ea0740c5c3b40de
Natan Borges <57094854+natanborges5@users.noreply.github.com>
2020-04-05 17:04:05 -0500

    header jogo

--- shc/src/Jogar/Jogar.vue
+++ shc/src/Jogar/Jogar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="backfix">
-    <Header/>
+    <HeaderJogo/>
   <br>
   <br>
   <br>
@@ -78,7 +78,7 @@
 
 <script>
 
-import Header from '../components/Header'
+import HeaderJogo from '../components/HeaderJogo'
 
 import { mapGetters, mapActions } from "vuex";
 
@@ -111,7 +111,7 @@ export default {
     this.fetchCards();
   },
   components: {
-    Header,
+    HeaderJogo,
  
     
   },
@@ -152,7 +152,6 @@ export default {
   display: flex;
   flex-wrap: wrap;
   padding: 10px;
-  margin-top: 30px;
   justify-content: center;
   
 }
@@ -162,6 +161,13 @@ export default {
 h1{
   font-family: "SSF4";
   font-size: 90px;
+}
+#logo{
+  width:45px;
+  margin-left: 0;
+  border-radius: 99%;
+  align-items:left;
+  transition: 0.5s;
 }
 .Cartas{
   display: flex;
